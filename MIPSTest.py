931a58ead5a701f39a8ff31bc9edb4cc9e31cc26
Challenging6 <992871471@qq.com>
2017-12-10 14:53:08 +0800

    test

--- MIPSTest.py
+++ MIPSTest.py
@@ -1,42 +1,30 @@
 import subprocess
 import os
 import re
-
-'''
-1.需要配合iverilog使用 下载地址:http://bleyer.org/icarus/ 并添加一下iverilog的环境变量(比如安装在C:\iverilog, 就将C:\iverilog\bin 添加到环境变量) 配合上届学长的升级版mars很好用
-
-升级版下载地址:https://github.com/depctg/Mockingbirds/blob/master/Mars.jar
-
-2.注意汇编不要写死循环,不然执行不出来(比如给的样例的机器码需要删除最后两行)
-
-3.请将测试脚本放到需要测试的project目录下(因为很多都默认的相对路径)
-
-4.需要在project里包含mips的testbench,需要在initial里包含以下两行: #10000; //表示仿真的时间 $finish; //表示结束仿真,不然不会结束
-
-5.最好不要包含其他无用的.v文件
-'''
+import logging
 
 
-project_location = "C:\\Users\Challenging\Desktop\p5"      #修改你的project地址(请将该脚本放到project目录下)
+
+
+project_location = "C:\\Users\Challenging\Desktop\p5\\"      #修改你的project地址(请将该脚本放到project目录下)
 mars_location = "C:\\Users\Challenging\Desktop\mars.jar"   #修改你的mars位置
 asm_location = "C:\\Users\Challenging\Desktop\mips1.asm"   #修改你的汇编地址
 print("执行汇编.........")
 
 Result = subprocess.getoutput("java -jar "+ mars_location +" db mc CompactDataAtZero nc "+asm_location)
-subprocess.getoutput("java -jar " + mars_location + " a dump .text HexText "+project_location+"\code.txt "+asm_location)
+subprocess.getoutput("java -jar " + mars_location + " a dump .text HexText "+project_location+"code.txt "+asm_location)
 marsResult = Result.strip().split("\n")
 marsResult = [item for item in marsResult if item[0:3]!="$ 0"]
 
-file = os.listdir(project_location)
-verilog = [i for i in file if i[-2:] == ".v"]
+files = os.listdir(project_location)
+verilog = [file for file in files if file[-2:] == ".v"]
 
-command = "iverilog"
-for i in verilog:
-    command=command+" "+ i
+os.chdir(project_location)
+command = "iverilog "
+for file in verilog:
+    command = command +file+" "
 
 print("开始仿真:"+command)
-
-
 subprocess.getoutput(command)
 answer = subprocess.getoutput("vvp a.out")
 
