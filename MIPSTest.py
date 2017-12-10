549df1636e70d30099ccf9cf56713a9b36b86b3d
Challenging666 <992871471@qq.com>
2017-12-10 14:54:43 +0800

    Update MIPSTest.py

--- MIPSTest.py
+++ MIPSTest.py
@@ -6,7 +6,7 @@ import logging
 
 
 
-project_location = "C:\\Users\Challenging\Desktop\p5\\"      #修改你的project地址(请将该脚本放到project目录下)
+project_location = "C:\\Users\Challenging\Desktop\p5\\"      #修改你的project地址(注意最后的斜杠不能丢!)
 mars_location = "C:\\Users\Challenging\Desktop\mars.jar"   #修改你的mars位置
 asm_location = "C:\\Users\Challenging\Desktop\mips1.asm"   #修改你的汇编地址
 print("执行汇编.........")
